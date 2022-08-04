b267f861da170314300eeb7bef17daabfca035a1
wkl007 <499657357@qq.com>
2022-08-04 16:56:30 +0800

    feat: loading tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,6 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
-use tauri::async_runtime::block_on;
 use tauri::{Runtime, AppHandle, Icon, SystemTray};
 
 fn tray_icon() -> Icon {
@@ -24,69 +23,17 @@ fn tray_icon_loading() -> Vec<Icon> {
   icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_9.png").to_vec()));
   icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_10.png").to_vec()));
   icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_11.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_12.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_13.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_14.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_15.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_16.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_17.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_18.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_19.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_20.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_21.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_22.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_23.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_24.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_25.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_26.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_27.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_28.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_29.png").to_vec()));
 
   return icon_vec;
 }
 
-fn set_default_icon<R: Runtime>(app: AppHandle<R>) -> Result<(), String> {
-  app.tray_handle().set_icon(tray_icon()).unwrap();
-  Ok(())
-}
-
-// 系统托盘图标 loading 动画效果采用的是定时器每隔 30 ms 切换图片实现的。
-// 现在碰到的问题是在切换回默认的托盘图标定时器没有销毁，还在一直调用。
-// 前端有 clearInterval 方法可以销毁定时器，但不知道 rust 是怎么处理的。
-async fn set_loading_icon<R: Runtime>(app: AppHandle<R>) -> Result<(), String> {
-  let ms = 30;
-  let icon_vec = tray_icon_loading();
-
-  let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(ms));
-  tokio::spawn(async move {
-    let mut i = 0;
-    loop {
-      interval.tick().await;
-      app.tray_handle().set_icon(icon_vec[i].clone()).unwrap();
-      i = if i >= 29 { 0 } else { i + 1 };
-      // force break for test
-      // if i >= 29 {
-      //   app.tray_handle().set_icon(tray_icon()).unwrap();
-      //   break;
-      // }
-    }
-  });
-  Ok(())
-}
-
 #[tauri::command]
-fn change_tray_icon<R: Runtime>(app: AppHandle<R>, icon_type: String) {
-  match icon_type.as_str() {
-    "default" => {
-      set_default_icon(app.clone()).unwrap();
-    }
-    "loading" => {
-      block_on(set_loading_icon(app.clone())).unwrap();
-    }
-    _ => {
-      set_default_icon(app.clone()).unwrap();
-    }
+fn change_tray_icon<R: Runtime>(app: AppHandle<R>, icon_type: String, icon_index: usize) {
+  if icon_type == "default" {
+    app.tray_handle().set_icon(tray_icon()).unwrap();
+  } else if icon_type == "loading" {
+    let icon_vec = tray_icon_loading();
+    app.tray_handle().set_icon(icon_vec[icon_index].clone()).unwrap();
   }
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useCallback } from 'react'
+import { useCallback, useRef } from 'react'
 import { invoke } from '@tauri-apps/api'
 import './App.css'
 
 function App () {
-  const changeTrayIcon = useCallback((iconType) => {
-    invoke('change_tray_icon', { iconType })
+  const interval = useRef()
+
+  const changeTrayIcon = useCallback(async (iconType) => {
+    try {
+      if (interval.current) clearInterval(interval.current)
+      if (iconType === 'default') {
+        await invoke('change_tray_icon', { iconType, iconIndex: 0 })
+      } else if (iconType === 'loading') {
+        let i = 0
+        interval.current = setInterval(async () => {
+          await invoke('change_tray_icon', { iconType, iconIndex: i })
+          if (i >= 11) {
+            i = 0
+          } else {
+            i += 1
+          }
+        }, 30)
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }, [])
 
   return (
